c8d59a71a2d1a6f5e8db8868fc796f1167003de7
sermariniandre <andre@sermarini.tech>
2024-03-16 10:55:57 -0300

    Projeto inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("leonardo")
+var Samantha = window.document.getElementById("samantha")
+var Bruna = window.document.getElementById("bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+
+function RolarParaDireita() {
+    Leonardo.style = "display: none"
+    Bruna.style = "display: flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display: flex; margin-top: 55px "
+}
+
+function RolarParaEsquerda(){
+    Leonardo.style = "display: flex"
+    Bruna.style = "display: none"
+    setaDireita.style = "display: flex"
+    setaEsquerda.style = "disney: none; margin-top: 55px"
+}
